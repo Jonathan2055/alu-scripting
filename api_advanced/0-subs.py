fe27bcf4e7f7dbe12dc252abe393bbeae1957be1
Jonathan MUGISHA <j.mugisha3@alustudent.com>
2025-07-14 21:59:18 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Gets number of subscribers for a given subreddit"""
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/"+subreddit+"/about.json"
