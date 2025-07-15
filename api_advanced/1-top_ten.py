192436f4a88ce94ae077cc504bef6554e01de667
Jonathan2055 <j.mugisha3@alustudent.com>
2025-07-15 11:42:47 +0000

    Error

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,13 +6,18 @@ import requests
 
 
 def top_ten(subreddit):
-    """ prints the titles of the first 10 hot posts listed in a subreddit """
+    """ Prints the titles of the first 10 hot posts listed in a subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
     headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
-        print(None)
-        return
-    posts = response.json()['data']['children']
-    for post in posts:
-        print(post['data']['title'])
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            return  # ✅ Do not print anything
+
+        posts = response.json().get('data', {}).get('children', [])
+        for post in posts:
+            print(post.get('data', {}).get('title'))
+
+    except Exception:
+        return  # ✅ Silently fail on errors
