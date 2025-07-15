aa95d5f7c47c30b161da556ade17892ba8742ec8
Jonathan2055 <j.mugisha3@alustudent.com>
2025-07-15 11:55:40 +0000

    Debug Error

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,22 +1,16 @@
-#!/usr/bin/python3
-"""Importing request modula"""
+"""Prints the titles of the top 10 hot posts of a subreddit"""
 import requests
 
-
 def top_ten(subreddit):
-    """Prints the first 10 hot post titles of a subreddit"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {"User-Agent": "alu-scripting:v1.0 (by /u/your_username)"}
-
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code != 200:
-            print("None")
-            return
-
-        data = response.json().get("data", {}).get("children", [])
-        for post in data:
-            print(post.get("data", {}).get("title"))
-
-    except Exception:
-        print("None")
+        if response.status_code == 200:
+            data = response.json().get("data", {}).get("children", [])
+            for post in data:
+                print(post.get("data", {}).get("title"))
+        else:
+            print(None)
+    except requests.RequestException:
+        print(None)
